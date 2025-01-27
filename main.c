cd3f53eb3e34c9d8cafbd6e0cffc4fbc8cedb5cb
kxrosen <alxjbecg@gmail.com>
2025-01-27 19:38:58 +0700

    source

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+	for(;;)
+	{
+		for(int i = 0; i <= 10; i++)
+		{
+			for(int j = 0; j <= 20; j++)
+			{
+				printf("#");
+			}
+
+			printf("\n");
+		}
+	}
+
+	return 0;
+}
